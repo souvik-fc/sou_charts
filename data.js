dbcf32cadfbf444ba1083aa59e376d0d1bd37ceb
souvik <souvik@fusioncharts.com>
2015-07-07 14:44:00 +0530

    click

---
+++ data.js
@@ -0,0 +1,48 @@
+var col=
+	       {
+	       	"width":700,
+	       	
+	       	"height":500,
+
+	       	"type" :'colm2d',
+
+	       	"xAxis":"MONTHS",
+
+	       	"yAxis":"TOTAL ATTENDANCE",
+	       	
+	       	"data":[
+	       	        {
+	       	        	"label":"JAN",
+	       	        	"val":210
+	       	        },
+	       	        {
+
+	       	        	"label":"FEB",
+	       	        	"val":110
+	       	        },
+	       	        {
+
+	       	        	"label":"MARCH",
+	       	        	"val":150
+	       	        },
+	       	        {
+
+	       	        	"label":"APRIL",
+	       	        	"val":920
+	       	        },
+	       	        {
+
+	       	        	"label":"MAY",
+	       	        	"val":30
+	       	        },
+	       	        {
+	       	        	"label":"JUNE",
+	       	        	"val":150
+	       	        },
+	       	        {
+	       	        	"label":"JULY",
+	       	        	"val":160
+	       	        }
+	       	       ]
+
+	        };
